901c8cc2910e04ed0413ebe80cdc3a438916219e
Dave-Desktop <pohl.david@gmail.com>
2023-01-05 07:17:23 -0600

    empty

--- pysg/TimeLapser.py
+++ pysg/TimeLapser.py
@@ -4,11 +4,14 @@ import PySimpleGUI as sg
 
 layout=[
 [sg.Text('Time Lapse Parameters:')],
-[sg.Text("Enter Time Lapse Interval"), sg.Combo([i for i in range(1,61)], key='-INTERVAL-', size=(4, 1), default_value=1), sg.Radio(text = "Seconds", group_id = "lapse_sec", default=True), sg.Radio(text = "Minutes", group_id = "lapse_sec")],
-[sg.Text("   Enter Capture Duration"), sg.Combo([i for i in range(1,61)], key='-DURATION-', size=(4, 1), default_value=1), sg.Radio(text = "Seconds", group_id = "duration_sec", default=True), sg.Radio(text = "Minutes", group_id = "duration_sec")],
-[sg.Text("   Enter Movie Frame Rate"), sg.Combo([i for i in range(1,26)], key='-FRAMERATE-', size=(4, 1), default_value=25), sg.Radio(text = "Seconds", group_id = "framerate_sec", default=True), sg.Radio(text = "Minutes", group_id = "framerate_sec")],
+[sg.Text("Enter Time Lapse Interval"), sg.Combo([i for i in range(1,61)], key='-INTERVAL-', size=(4, 1), default_value=1), sg.Radio(text = "Seconds", group_id = "lapse_sec", default=True, key=("-LAPSE_SECONDS-")), sg.Radio(text = "Minutes", group_id = "lapse_sec", key=("-LAPSE_MINUTES-"))],
+[sg.Text("   Enter Capture Duration"), sg.Combo([i for i in range(1,61)], key='-DURATION-', size=(4, 1), default_value=10), sg.Radio(text = "Seconds", group_id = "duration_sec", default=True, key=("-DURATION_SECONDS-")), sg.Radio(text = "Minutes", group_id = "duration_sec", key=("-DURATION_MINUTES-"))],
+[sg.Text("   Enter Movie Frame Rate"), sg.Combo([i for i in range(1,26)], key='-FRAMERATE-', size=(4, 1))],
+[sg.Text("Total Frames to Capture: "), sg.Text("frames", key='-FRAMES_CAPTURED-')],
 [sg.Text("Resulting Movie Length: "), sg.Text("idk", key='-MOVIE_LENGHT-')],
+[sg.Button('Calculate')],
 [sg.Button('OK')]
+
 ]
 
 # Create the window
@@ -21,7 +24,21 @@ while True:
     # presses the OK button
     print(event, values)
     if event == "OK" or event == sg.WIN_CLOSED:
+        break
+    if event == "Calculate":
+        interval = values['-INTERVAL-']
+        if values["-LAPSE_MINUTES-"]: # minutes radio selected
+            interval = interval * 60
+        print("interval", interval)
 
-        window['-MOVIE_LENGHT-'].update(values['-INTERVAL-']*values['-DURATION-'])
+    if event == "Calculate":
+        duration = values['-DURATION-']
+        if values["-DURATION_MINUTES-"]: # minutes radio selected
+            duration = duration * 60
+        print("duration", duration)
 
+    
+        total_frames =  values['-INTERVAL-']*values['-DURATION-']
+        window['-MOVIE_LENGHT-'].update()
+        window['-FRAMES_CAPTURED-'].update(values['-INTERVAL-']*values['-DURATION-'])
 window.close()
